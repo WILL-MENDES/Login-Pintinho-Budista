808b8629252db4fe01f1334cbe2cc33823fff954
William Mendes <william3mendes3@gmail.com>
2023-12-20 15:32:48 -0300

    criação de tela de agradecimento

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ function ativaModal(){
 
 function desativaModal(){
     const modal = document.querySelector('#corpoModal');
-    alert(modal)
     document.addEventListener('click', function(event){
         if (event.target === corpoModal){
             corpoModal.style.display = 'none'
@@ -13,3 +12,27 @@ function desativaModal(){
 }
 
 desativaModal()
+
+function cadastroRealizado(){
+ 
+    const inputNome = document.getElementById('caixa1')
+    const inputSobrenome = document.getElementById('caixa2')
+    const inputEmail = document.getElementById('caixa3')
+    const inputSenha = document.getElementById('caixa4')
+  
+    
+    let nome = inputNome.value;
+    let sobrenome = inputSobrenome.value;
+    let email = inputEmail.value;
+    let senha = inputSenha.value;
+
+ if (nome.length >= 3 && sobrenome.length >= 3 && email.length >= 13 && senha.length >= 8 ){
+
+    $('.cadastro').children().remove();
+    $('.cadastro').append('<h2 class="obrigado">Obrigado</h2>');
+
+   
+    
+ } else ( alert('Nome e Sobrenome acima de 3 letras / email valido / senha somente com mais de 8 caracteres'));
+
+}
